88d76e7a70c904c7debeb915a7fe09843408799d
mateusscavalheiro-afk <mateus_s_cavalheiro@estudante.sesisenai.org.br>
2026-02-10 14:07:01 -0300

    Update Main.java

--- src/main/java/Logica_Classes_Automacao/Main.java
+++ src/main/java/Logica_Classes_Automacao/Main.java
@@ -1,5 +1,7 @@
 package Logica_Classes_Automacao;
 
+@SuppressWarnings("all")
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("==========================================");
